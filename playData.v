f19315191eff720cd63f47a53d31a09344e3d8ec
ZhanLu00 <zhanlu0523@gmail.com>
2019-07-22 11:13:52 -0400

    tested player movement

--- playData.v
+++ playData.v
@@ -1,51 +1,39 @@
 module playData(
 	input ld_1, ld_2, ld_3, ld_4,
 	input clock, reset, erase, draw,
-	input [2:0] color_in,
 	output [7:0] x_out,
 	output [6:0] y_out,
 	output [2:0] color_out);
 
 	reg [7:0] xout;
-   //reg [6:0] yout;
 	reg [2:0] cout;
-	wire [7:0] boxX;
-	wire [6:0] boxY;
 	
-    assign color_out = cout;
+    	
 	 
 	localparam	x1 = 8'd14,
-				x2 = 8'd54,
-				x3 = 8'd94,
-				x4 = 8'd134,
-				y  = 7'd99,
-				COLOR = 3'b111;
+			x2 = 8'd54,
+			x3 = 8'd94,
+			x4 = 8'd134,
+			y  = 7'd99,
+			COLOR = 3'b111;
 
 	always @(posedge clock)
 	begin
-		if (erase || !reset) begin cout <= 3'b000; xout <= x1; end
-		else begin 
-			//if (draw) begin
-				if (ld_1) begin xout <= x1; cout <= COLOR; end
-				if (ld_2) begin xout <= x2; cout <= COLOR; end
-				if (ld_3) begin xout <= x3; cout <= COLOR; end
-				if (ld_4) begin xout <= x4; cout <= COLOR; end
-			//end
+		if (!reset) begin cout <= COLOR; xout <= x1; end // default for reset,  c =1, x = x1
+		else if (erase) begin cout <= 3'b0; xout <= xout; end // erase: color = 0, x = the last stored position
+		else if (draw) begin 
+			if (ld_1) begin xout <= x1; cout <= COLOR; end
+			if (ld_2) begin xout <= x2; cout <= COLOR; end
+			if (ld_3) begin xout <= x3; cout <= COLOR; end
+			if (ld_4) begin xout <= x4; cout <= COLOR; end
+			
 		end
+
 	end
 
-
-	drawBox p(
-		.xin(xout),
-		.yin(y),
-		.reset_N(reset),
-		.clk(clock),
-		.x_out(boxX),
-		.y_out(boxY)
-				
-	);
 	
-	assign x_out = boxX;
-	assign y_out = boxY;
+	assign x_out = xout;
+	assign y_out = y;
+	assign color_out = cout;
 
 endmodule
--- playerCom.v
+++ playerCom.v
@@ -1,3 +1,47 @@
+module playerMovement(clock, resetn, aP, dP, x_out, y_out);
+    input clock, resetn, aP, dP;
+	output [7:0] x_out;
+	output [6:0] y_out;
+    wire [7:0] x;
+    wire [6:0] y;
+    wire [2:0] colour_out;
+
+    wire ld_pos1, ld_pos2, ld_pos3, ld_pos4, inEraseState, inDrawState;
+
+    playerMovementFSM c1(
+        .clk(clock),
+        .resetn(resetn), 
+        .ld_pos1(ld_pos1), 
+        .ld_pos2(ld_pos2), 
+        .ld_pos3(ld_pos3), 
+        .ld_pos4(ld_pos4), 
+        .inEraseState(inEraseState), 
+        .inDrawState(inDrawState), 
+        .aPressed(aP), 
+        .dPressed(dP)
+    );
+    
+    
+
+    playData p1(
+        .ld_1(ld_pos1),
+        .ld_2(ld_pos2),
+        .ld_3(ld_pos3),
+        .ld_4(ld_pos4),
+	    .clock(clock), 
+        .reset(resetn),
+        .erase(inEraseState),
+        .draw(inDrawState),
+	    .x_out(x),
+	    .y_out(y),
+	    .color_out(colour_out)
+    );
+
+	assign x_out = x;
+	assign y_out = y;
+    
+endmodule   
+
 module playerMovementFSM(clk, resetn, ld_pos1, ld_pos2, ld_pos3, ld_pos4, inEraseState, inDrawState, aPressed, dPressed);
 	input clk, resetn, aPressed, dPressed;
 	
@@ -109,5 +153,45 @@ module playerMovementFSM(clk, resetn, ld_pos1, ld_pos2, ld_pos3, ld_pos4, inEras
         end
     end // state_FFS
 
+
+endmodule
+
+module playData(
+	input ld_1, ld_2, ld_3, ld_4,
+	input clock, reset, erase, draw,
+	output [7:0] x_out,
+	output [6:0] y_out,
+	output [2:0] color_out);
+
+	reg [7:0] xout;
+	reg [2:0] cout;
+	
+    	
+	 
+	localparam	x1 = 8'd14,
+			x2 = 8'd54,
+			x3 = 8'd94,
+			x4 = 8'd134,
+			y  = 7'd99,
+			COLOR = 3'b111;
+
+	always @(posedge clock)
+	begin
+		if (!reset) begin cout <= COLOR; xout <= x1; end // default for reset,  c =1, x = x1
+		else if (erase) begin cout <= 3'b0; xout <= xout; end // erase: color = 0, x = the last stored position
+		else if (draw) begin 
+			if (ld_1) begin xout <= x1; cout <= COLOR; end
+			if (ld_2) begin xout <= x2; cout <= COLOR; end
+			if (ld_3) begin xout <= x3; cout <= COLOR; end
+			if (ld_4) begin xout <= x4; cout <= COLOR; end
+			
+		end
+
+	end
+
+	
+	assign x_out = xout;
+	assign y_out = y;
+	assign color_out = cout;
 
 endmodule
