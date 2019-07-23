f20f79278d27526bc98e191cd2a17e9612706f24
Viktar Chyhir <vchigir12@gmail.com>
2019-07-23 13:22:13 -0400

    Made new branch for implementing the drawin logic

---
+++ CodeOverhaul/DrawingCode/draw.v
@@ -0,0 +1,41 @@
+module draw(
+    input [7:0] x_in,
+    input [6:0] y_in,
+    input [4:0] width, height,
+    input [2:0] c_in,
+    input draw, clk, reset,
+    output [7:0] x_out,
+    output [6:0] y_out,
+    output [2:0] c_out,
+    output done
+);
+    reg [7:0] counterX, xOut; 
+    reg [6:0] counterY, yOut;
+    reg done_;
+
+
+    always @(posedge clk)
+    begin
+        if (!reset) begin
+            xOut <= x_in;
+            yOut <= y_in;
+            done_ <= 0;
+        end
+        else if (draw) begin
+            if (counterX == (width-1)) begin
+                counterX <= 0; 
+                counterY <= counterY + 1; end
+            else if (counterX < (width-1)) begin
+                counterX <= counterX + 1; end
+
+            if (counterY == height) begin
+                done_ <= 1'b1; end
+        end
+
+    end
+
+    assign x_out = xOut + counterX;
+    assign y_out = yOut + counterY;
+    assign done = done_;
+
+endmodule
---
+++ CodeOverhaul/DrawingCode/drawFSM.v
@@ -0,0 +1,76 @@
+module drawFSM(
+    input done, clk, resetn,
+    output [3:0] mainDrawSignal);
+
+
+    //output drawPlayer, drawEnemy1, drawEnemy2, drawEnemy3, drawEnemy4, drawBullet;
+
+
+    reg [3:0] drawSignalOut;
+
+    output enable;
+    assign enable=1;
+
+    reg [3:0] current_state, next_state;
+
+    localparam S_DRAW_PLAYER = 4'd0,
+               S_DRAW_ENEMY1 = 4'd1,
+               S_DRAW_ENEMY2 = 4'd2,
+               S_DRAW_ENEMY3 = 4'd3,
+               S_DRAW_ENEMY4 = 4'd4,
+               S_DRAW_BULLET = 4'd5;
+
+    always@(*)
+    begin: state_table 
+            case (current_state)           
+                S_DRAW_PLAYER: next_state = (done == 1'b1) ? S_DRAW_ENEMY1 : S_DRAW_PLAYER;
+                S_DRAW_ENEMY1: next_state = (done == 1'b1) ? S_DRAW_ENEMY2 : S_DRAW_ENEMY1;
+                S_DRAW_ENEMY2: next_state = (done == 1'b1) ? S_DRAW_ENEMY3 : S_DRAW_ENEMY2;
+                S_DRAW_ENEMY3: next_state = (done == 1'b1) ? S_DRAW_ENEMY4 : S_DRAW_ENEMY3;
+                S_DRAW_ENEMY4: next_state = (done == 1'b1) ? S_DRAW_BULLET : S_DRAW_ENEMY4;
+                S_DRAW_BULLET: next_state = (done == 1'b1) ? S_DRAW_PLAYER : S_DRAW_BULLET;
+
+
+
+            default:     next_state = S_DRAW_PLAYER;
+        endcase
+    end // state_table
+
+
+    always @(*)
+    begin: enable_signals
+        // By default make all our signals 0
+       // drawPlayer = 1'b0;
+        //drawEnemy1 = 1'b0;
+        //drawEnemy2 = 1'b0;
+        //drawEnemy3 = 1'b0;
+        //drawEnemy4 = 1'b0;
+        //drawBullet = 1'b0;
+        drawSignalOut <= 4'b0;
+
+        case (current_state)
+            S_DRAW_PLAYER: drawSignalOut <= 4'd0;
+            S_DRAW_ENEMY1: drawSignalOut <= 4'd1;
+            S_DRAW_ENEMY2: drawSignalOut <= 4'd2;
+            S_DRAW_ENEMY3: drawSignalOut <= 4'd3;
+            S_DRAW_ENEMY4: drawSignalOut <= 4'd4;
+            S_DRAW_BULLET: drawSignalOut <= 4'd5;
+				
+        // default:    // don't need default since we already made sure all of our outputs were assigned a value at the start of the always block
+        endcase
+    end // enable_signals
+
+    always@(posedge clk)
+    begin: state_FFs
+        if (resetn == 1'b0) begin
+            current_state <= S_DRAW_PLAYER;
+        end
+        else 
+            current_state <= next_state;
+	
+       
+    end // state_FFS
+
+    assign mainDrawSignal = drawSignalOut;
+
+endmodule
